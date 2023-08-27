2ca14aa4114230d6d5725d1be8275174306f243e
ShirvanyanKaren <kshirvanyan2000@gmail.com>
2023-08-26 22:42:11 -0700

    fixed install

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,17 +4,26 @@ const butInstall = document.getElementById('buttonInstall');
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
+    window.deferredPrompt = event;
     butInstall.style.visibility = 'visible';
     butInstall.textContent = "Install Me!";
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
+    const promptEv = window.deferredPrompt;
+    if(!promptEv){
+        return;
+    }
+    promptEv.prompt();
+    window.deferredPrompt = null;
     butInstall.setAttribute('disabled', true);
-    butInstall.textContent = 'Installed!';
+    
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+    butInstall.textContent = 'Installed!';
     console.log('🤙','installed', event);
 });
